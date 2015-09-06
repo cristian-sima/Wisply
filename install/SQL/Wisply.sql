9be4df5e002a99c5f57bb6a7e1a3c17d8fe8679e
cristian-sima <cs25g13@soton.ac.uk>
2015-09-06 22:45:58 +0100

    New version of installer on Ubuntu until step 4

---
+++ install/SQL/Wisply.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 06, 2015 at 11:39 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+USE `Wisply`
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wisply`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `source`
+--
+
+CREATE TABLE IF NOT EXISTS `source` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `url` varchar(2083) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `source`
+--
+
+INSERT INTO `source` (`id`, `name`, `description`, `url`) VALUES
+(34, 'University of Southampton', '', 'htfym.ghghjtp://soton.ac.uk'),
+(35, 'assadasd', '', 'http://vsh.ro'),
+(36, 'ada', '', 'http://vsh.ro');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `source`
+--
+ALTER TABLE `source`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `source`
+--
+ALTER TABLE `source`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=37;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+database="Wisply"
+MySQLUsername="root"
+MySQLPassword="Pv1XL_De_zHdhgjWu"
+databaseUsername="root"
+databasePassword="root"
+#--------------------------------Messages --------------------------------------
 showMargin () {
    echo "|---------------------------------------------------------------------|"
 }
@@ -18,8 +24,13 @@ showError () {
     showBlankLine
     showMargin
     echo
+    exit 0
+}
+showWarning () {
+  showMessage "[Warning] $1"
 }
 showHeading () {
+  showBlankLine
   showMessage "$1.$2"
   showBlankLine
 }
@@ -32,12 +43,28 @@ showInstaller () {
 showHappyEnd () {
     showBlankLine
     showBlankLine
-    showMessage "The script has been sucessfully executed and this is the end of installer."
+    showMessage "The installer has been sucessfully executed!"
     showMessage "Have a nice day!"
+    showBlankLine
+    showBlankLine
 }
-#----------------------------------------------------------------------------
-
-# ---------------------------------------------------------------------------
+#---------------------------------- MySQL ----------------------------------
+requestMySQLCredentials () {
+  showMessage "Please type the username for MySQL (by default it is root):"
+  read MySQLUsername
+  showMessage "Please type the password for MySQL username $user:"
+  read MySQLPassword
+  showMessage "Thanks!"
+}
+verifySQLCredentials () {
+  if ! mysql -u "$MySQLUsername" -p"$MySQLPassword" -e "quit";
+  then
+    showError "The username and password are not good. Try again!"
+  else
+    showSuccess "The user and the password are good"
+  fi
+}
+# --------------------------------- Database ----------------------------------
 
 isMysqlInstalled () {
   showMessage "- Checking MySQL..."
@@ -49,30 +76,98 @@ isMysqlInstalled () {
     return 1
   fi
 }
-executeSQLFile () {
-  dbname = "Wisply"
-  showMessage "Please type the username of the database"
-  read user
-  showMessage "Please type the password for the username $user"
-  read password
-  showMessage "Please wait..."
-  mysql -u user -p password dbname  Wisply.sql
-  showSuccess "The SQL script has been executed"
+deleteDatabase () {
+  showMessage "Trying to delete the previous database..."
+  showMessage "We need your permission!"
+  if mysqladmin -u"$MySQLUsername" -p"$MySQLPassword" drop "$database";
+  then
+    showSuccess "The previous database has been deleted"
+  else
+    showError "The previous database has not been deleted"
+  fi
+}
+checkIfDatabaseExists () {
+  showMessage "Checking if there is any previous database with the name $database..."
+  if mysql -u"$MySQLUsername" -p"$MySQLPassword" -e "use $database";
+  then
+      return 1
+  else
+      return 0
+  fi
+}
+deletePreviousDatabase () {
+  checkIfDatabaseExists
+  databaseStatus=$?
+  doesNotExists=0
+  if [ $databaseStatus != $doesNotExists ];
+  then
+   showWarning "A previous database already exists"
+   deleteDatabase
+  else
+   showSuccess "No database with the name $database"
+  fi
 }
 createDatabase () {
-  mysql -u root -e "create database testdb";
+  echo "create database $database" | mysql -u "$MySQLUsername" -p"$MySQLPassword"
+  showSuccess "The database $database has been created!"
 }
 setUpDatabase () {
-  showHeading "1" "Set up database"
+  showHeading "2" "Set up database"
+
+  # receive credentials
+  requestMySQLCredentials
+
+  # check credentials are good
+  verifySQLCredentials
+
   # check and delete any Wisply database
+  deletePreviousDatabase
 
-  # check database
-
-  # create user for database with all rights
-
-  # execute script
+  # create database
+  createDatabase
 }
-start () {
+#----------------------------------- Username ---------------------------------
+requestUsernameCredentials () {
+  showMessage "Type the name of username:"
+  read databaseUsername
+  showMessage "Type the password of username:"
+  read databasePassword
+  showMessage "Thanks!"
+}
+createDatabaseUsername () {
+  Q2="GRANT USAGE ON *.* TO $databaseUsername@localhost IDENTIFIED BY '$databasePassword';"
+  Q3="GRANT ALL PRIVILEGES ON $database.* TO $databaseUsername@localhost;"
+  Q4="FLUSH PRIVILEGES;"
+  SQL="${Q2}${Q3}${Q4}"
+
+  if mysql -u"$MySQLUsername" -p"$MySQLPassword" -e "$SQL";
+  then
+    showSuccess "The username has been created."
+  else
+    showError "Problems while creating the username"
+  fi
+}
+#----------------------------------- Start ------------------------------------
+executeSQLFile () {
+  SQLFile="SQL/Wisply.sql"
+  if mysql -u"$MySQLUsername" -p"$MySQLPassword" < "$SQLFile";
+  then
+    showSuccess "The database has been populated"
+  else
+    showError "There was an error while executing the SQL script"
+  fi
+}
+populateDatabase () {
+  showHeading "4" "Populating database"
+  showMessage "Please wait..."
+  executeSQLFile
+}
+setUsername () {
+  showHeading "3" "Database username"
+  requestUsernameCredentials
+  createDatabaseUsername
+}
+checkServer () {
   showHeading "1" "Starting the work"
   notInstalled=0
   isMysqlInstalled
@@ -83,8 +178,13 @@ start () {
     showError "The MySQL server is not installed. Please see the tutorial here: \n $link"
   else
     showSuccess "MySQL is installed."
-    setUpDatabase
   fi
+}
+start () {
+  checkServer
+  setUpDatabase
+  setUsername
+  populateDatabase
 }
 startInstaller () {
   showInstaller

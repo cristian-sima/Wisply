507149fc0fe787539d2f214db3282def7e7bf27a
cristian-sima <cs25g13@soton.ac.uk>
2015-09-14 18:05:42 +0100

    Added image for installer example

--- tests/default_test.go
+++ tests/default_test.go
@@ -1,77 +1,38 @@
 package test
 
 import (
-	"github.com/astaxie/beego"
-	_ "github.com/cristian-sima/Wisply/routers"
-	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
 	"net/http/httptest"
-	"path/filepath"
-	"runtime"
 	"testing"
+	"runtime"
+	"path/filepath"
+	"github.com/cristian-sima/Wisply/routers"
+
+	"github.com/astaxie/beego"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
 	_, file, _, _ := runtime.Caller(1)
-	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
 	beego.TestBeegoInit(apppath)
 }
 
-func TestPageHome(t *testing.T) {
-	url := "/"
-	name := "Home"
-	checkPage(t, url, name)
-}
 
-func TestPageSample(t *testing.T) {
-	url := "/sample"
-	name := "Sample"
-	checkPage(t, url, name)
-}
-
-func TestPageAbout(t *testing.T) {
-	url := "/about"
-	name := "About"
-	checkPage(t, url, name)
-}
-
-func TestPageWebscience(t *testing.T) {
-	url := "/webscience"
-	name := "Webscience"
-	checkPage(t, url, name)
-}
-
-func TestPageContact(t *testing.T) {
-	url := "/contact"
-	name := "Contact"
-	checkPage(t, url, name)
-}
-
-func TestPageLogin(t *testing.T) {
-	url := "/auth/login"
-	name := "Login"
-	checkPage(t, url, name)
-}
-
-func TestPageRegister(t *testing.T) {
-	url := "/auth/admin"
-	name := "Register"
-	checkPage(t, url, name)
-}
-
-func checkPage(t *testing.T, url string, pageName string) {
-	r, _ := http.NewRequest("GET", url, nil)
+// TestMain is a sample to run an endpoint test
+func TestMain(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("Test static page: ", pageName)
+	beego.Trace("testing", "TestMain", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
-		Convey("The page "+pageName+" can be accessed", func() {
-			So(w.Code, ShouldEqual, 200)
-		})
-		Convey("The page should not be empty and pass HTML check", func() {
-			So(w.Body.Len(), ShouldBeGreaterThan, 0)
-		})
+	        Convey("Status Code Should Be 200", func() {
+	                So(w.Code, ShouldEqual, 200)
+	        })
+	        Convey("The Result Should Not Be Empty", func() {
+	                So(w.Body.Len(), ShouldBeGreaterThan, 0)
+	        })
 	})
 }

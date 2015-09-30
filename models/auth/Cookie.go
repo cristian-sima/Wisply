f952df8cd8bb20c012facd6dc5b4697bd7fc2894
cristian-sima <cs25g13@soton.ac.uk>
2015-09-30 20:20:53 +0100

    Added unit tests for auth

--- models/auth/Cookie.go
+++ models/auth/Cookie.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"errors"
 	"strconv"
+	"strings"
+
+	wisply "github.com/cristian-sima/Wisply/models/database"
 )
 
 // Cookie It represents a connection cookie
@@ -17,4 +21,63 @@ type Cookie struct {
 // The value is in this format: ID::EncryptedToken
 func (cookie *Cookie) GetValue() string {
 	return strconv.Itoa(cookie.Account.ID) + "::" + cookie.Token.Encrypt()
+}
+
+// LoginCookie represents a cookie for login
+// It is used to remember the user for a long period of time
+type LoginCookie struct {
+	Token     string
+	AccountID string
+}
+
+// IsGood creates a Token from the id and hashed token and calls its IsValid method
+func (cookie *LoginCookie) IsGood() bool {
+
+	if wisply.Database == nil {
+		return false
+	}
+
+	token, err := newTokenFromCookie(cookie)
+
+	if err != nil {
+		return false
+	}
+	isValid := token.IsValid()
+	return isValid
+}
+
+func newLoginCookie(plainCookie string) (*LoginCookie, error) {
+	var cookie *LoginCookie
+
+	if !hasGoodFormat(plainCookie) {
+		return cookie, errors.New("The cookie does not have a valid format")
+	}
+	elements := getCookieElements(plainCookie)
+
+	cookie = &LoginCookie{
+		AccountID: elements[0],
+		Token:     elements[1],
+	}
+
+	return cookie, nil
+}
+
+func hasGoodFormat(plainCookie string) bool {
+	separator := Settings["separatorCookie"].(string)
+	containsSeparator := strings.Contains(plainCookie, separator)
+	if !containsSeparator {
+		return false
+	}
+	elements := getCookieElements(plainCookie)
+	if len(elements) != 2 {
+		return false
+	}
+	return true
+}
+
+func getCookieElements(plainCookie string) []string {
+	var elements []string
+	separator := Settings["separatorCookie"].(string)
+	elements = strings.Split(plainCookie, separator)
+	return elements
 }
--- models/auth/Token.go
+++ models/auth/Token.go
@@ -1,5 +1,12 @@
 package auth
 
+import (
+	"fmt"
+	"strconv"
+
+	wisply "github.com/cristian-sima/Wisply/models/database"
+)
+
 // Token The token is a combination of a hashed unique random number and a Timestamp
 // It is used to remember an account for a long period
 // It is stored in the cookie "connection" along with the ID of the account
@@ -11,4 +18,25 @@ type Token struct {
 // Encrypt It encrypts the value of the token
 func (token *Token) Encrypt() string {
 	return getSHA1Digest(token.Value)
+}
+
+// IsValid checks if the token has not expired
+func (token *Token) IsValid() bool {
+	var isValid bool
+	now, _ := strconv.Atoi(getCurrentTimestamp())
+	duration := Settings["duration"].(int)
+	fmt.Println(strconv.Itoa(now - duration - token.Timestamp))
+	if token.Timestamp+duration >= now {
+		isValid = true
+	}
+	return isValid
+}
+
+// It creates a new token using a LoginCookie
+func newTokenFromCookie(cookie *LoginCookie) (*Token, error) {
+	token := &Token{}
+	sql := "SELECT value, timestamp FROM account_token WHERE account=? AND value=?"
+	query, err := wisply.Database.Prepare(sql)
+	query.QueryRow(cookie.AccountID, cookie.Token).Scan(&token.Value, &token.Timestamp)
+	return token, err
 }

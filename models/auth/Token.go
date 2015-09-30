f9fd2ed572fe8c22fc9ca1a2e183f9647ff2de0e
cristian-sima <cs25g13@soton.ac.uk>
2015-09-30 20:46:54 +0100

    Refactored account

--- models/auth/Token.go
+++ models/auth/Token.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	wisply "github.com/cristian-sima/Wisply/models/database"
+	"github.com/nu7hatch/gouuid"
 )
 
 // Token The token is a combination of a hashed unique random number and a Timestamp
@@ -13,11 +14,19 @@ import (
 type Token struct {
 	Value     string
 	Timestamp int
+	Account   *Account
 }
 
 // Encrypt It encrypts the value of the token
 func (token *Token) Encrypt() string {
 	return getSHA1Digest(token.Value)
+}
+
+// Insert inserts the token in the database
+func (token *Token) Insert() {
+	sql := "INSERT INTO `account_token` (`id`, `account`, `value`, `timestamp`) VALUES (?, ?, ?, ?)"
+	query, _ := wisply.Database.Prepare(sql)
+	query.Exec("NULL", strconv.Itoa(token.Account.ID), token.Encrypt(), token.Timestamp)
 }
 
 // IsValid checks if the token has not expired
@@ -39,4 +48,19 @@ func newTokenFromCookie(cookie *LoginCookie) (*Token, error) {
 	query, err := wisply.Database.Prepare(sql)
 	query.QueryRow(cookie.AccountID, cookie.Token).Scan(&token.Value, &token.Timestamp)
 	return token, err
+}
+
+func generateToken(account *Account) *Token {
+
+	randomNumber, _ := uuid.NewV4()
+	plainValue := randomNumber.String()
+	timestamp := getCurrentTimestamp()
+
+	intTimestamp, _ := strconv.Atoi(timestamp)
+	token := Token{
+		Value:     plainValue,
+		Timestamp: intTimestamp,
+		Account:   account,
+	}
+	return &token
 }

df0851d29de036349bc1cbcf0c667e2db0d205b9
cristian-sima <cs25g13@soton.ac.uk>
2015-10-31 21:02:26 +0000

    Revert "Fix conflict"
    This reverts commit b12d40c9e38eb961c913bb0ffc698168214b7f97.

---
+++ models/auth/search/list.go
@@ -0,0 +1,76 @@
+package searches
+
+import "github.com/cristian-sima/Wisply/models/database"
+
+// List encapsulates all the searches made by an account
+type List struct {
+	accountID int
+}
+
+// InsertAccessed adds a query that was accessed by the account
+// It sets the timestamp
+func (list List) InsertAccessed(query string) {
+	list.insertQuery(query, true)
+}
+
+// InsertNotAccessed adds a query that was not accessed by account
+// It sets the timestamp
+func (list List) InsertNotAccessed(query string) {
+	list.insertQuery(query, false)
+}
+
+// Insert adds the search into database
+// It sets the timestamp
+func (list List) insertQuery(query string, accessedBool bool) {
+	var getAccessedString = func(accessed bool) string {
+		if accessed {
+			return "1"
+		}
+		return "0"
+	}
+	timestamp := getCurrentTimestamp()
+	fieldList := "`query`, `timestamp`, `account`, `accessed`"
+	questionList := "?, ?, ?, ?"
+	accessedString := getAccessedString(accessedBool)
+	sql := "INSERT INTO `account_search` (" + fieldList + ") VALUES (" + questionList + ")"
+	stmt, _ := database.Connection.Prepare(sql)
+	stmt.Exec(query, timestamp, list.accountID, accessedString)
+}
+
+// Clear clears the history of a user
+func (list List) Clear() {
+	sql := "DELETE FROM `account_search` WHERE account = ? "
+	stmt, _ := database.Connection.Prepare(sql)
+	stmt.Exec(list.accountID)
+}
+
+// GetAll returns the list of all
+func (list List) GetAll() []Search {
+	var allList []Search
+	var getBoolFromString = func(accessed string) bool {
+		if accessed == "1" {
+			return true
+		}
+		return false
+	}
+	fieldList := "`id`, `query`, `timestamp`, `accessed`"
+	whereClause := "WHERE `account` = ?"
+	orderClause := "ORDER BY `timestamp` DESC "
+	sql := "SELECT " + fieldList + "FROM `account_search` " + whereClause + " " + orderClause
+	rows, _ := database.Connection.Query(sql, list.accountID)
+	for rows.Next() {
+		accessedString := ""
+		item := Search{}
+		rows.Scan(&item.ID, &item.Query, &item.Timestamp, &accessedString)
+		item.Accessed = getBoolFromString(accessedString)
+		allList = append(allList, item)
+	}
+	return allList
+}
+
+// NewList loads a searches object for an account
+func NewList(accountID int) List {
+	return List{
+		accountID: accountID,
+	}
+}

262db53326480dad71c8651c723fe926ee6bd35f
cristian-sima <cristian-sima@users.noreply.github.com>
2015-10-31 21:09:08 +0000

    Delete Message.go

--- models/ws/Message.go
+++
@@ -1,8 +0,0 @@
-package websockets
-
-// Message represents a message from client to server
-type Message struct {
-	Name       string      `json:"Name"`
-	Repository int         `json:"Repository"` // Repository represents the ID of the repository
-	Value      interface{} `json:"Value"`
-}
